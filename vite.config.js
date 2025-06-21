18f3d893a5f9487f80bac35a9af3f5d26169240b
Aishwarya Ghatole <aishwaryaghatole8@gmail.com>
2025-06-21 22:35:43 +0530

    Fix routing for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 export default defineConfig({
   plugins: [react()],
-  define:{
+  define: {
     // .env variables
-    'process.env.VITE_EMAILJS_SERVICE_ID' : JSON.stringify(process.env.VITE_EMAILJS_SERVICE_ID),
-    'process.env.VITE_EMAILJS_TEMPLATE_ID' : JSON.stringify(process.env.VITE_EMAILJS_TEMPLATE_ID),
-    'process.env.VITE_EMAILJS_PUBLIC_KEY' : JSON.stringify(process.env.VITE_EMAILJS_PUBLIC_KEY),  
-  }, 
-  base: "/aishwarya-portfolio",
+    "process.env.VITE_EMAILJS_SERVICE_ID": JSON.stringify(
+      process.env.VITE_EMAILJS_SERVICE_ID
+    ),
+    "process.env.VITE_EMAILJS_TEMPLATE_ID": JSON.stringify(
+      process.env.VITE_EMAILJS_TEMPLATE_ID
+    ),
+    "process.env.VITE_EMAILJS_PUBLIC_KEY": JSON.stringify(
+      process.env.VITE_EMAILJS_PUBLIC_KEY
+    ),
+  },
 });
