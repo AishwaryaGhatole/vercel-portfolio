014a304c2b1f56b7ea1481d766d796815910cf15
Aishwarya Ghatole <aishwaryaghatole8@gmail.com>
2026-01-22 20:13:32 +0530

    resume update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
 
   const toggleResume = () => {
-    const resumeUrl = "/resume1.pdf";
+    const resumeUrl = "/resume.pdf";
     window.open(resumeUrl, "_blank");
   };
 
